929c00614e03f5d8d92e592b90a5d7a1a7475f3d
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-03-27 01:12:07 -0700

    initial commit of FaceTracker video projection project

---
+++ src/testApp.cpp
@@ -0,0 +1,89 @@
+#include "testApp.h"
+
+using namespace ofxCv;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	ofSetVerticalSync(true);
+	ofSetDrawBitmapMode(OF_BITMAPMODE_MODEL_BILLBOARD);
+	cam.initGrabber(640, 480);
+	
+	tracker.setup();
+	tracker.setRescale(.5);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	cam.update();
+	if(cam.isFrameNew()) {
+		tracker.update(toCv(cam));
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetColor(255);
+	cam.draw(0, 0);
+	ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
+	
+	ofPolyline leftEye = tracker.getImageFeature(ofxFaceTracker::LEFT_EYE);
+	ofPolyline rightEye = tracker.getImageFeature(ofxFaceTracker::RIGHT_EYE);
+	ofPolyline faceOutline = tracker.getImageFeature(ofxFaceTracker::FACE_OUTLINE);
+	
+	ofSetLineWidth(2);
+	ofSetColor(ofColor::red);
+	leftEye.draw();
+	ofSetColor(ofColor::green);
+	rightEye.draw();
+	ofSetColor(ofColor::blue);
+	faceOutline.draw();
+	
+	ofSetLineWidth(1);
+	ofSetColor(255);
+	tracker.draw();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+	tracker.reset();
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+	
+}
